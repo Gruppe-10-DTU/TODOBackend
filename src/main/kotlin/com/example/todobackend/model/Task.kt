0cfb46456bc00f6077d2b43ca7392cd10b81b895
sand4570 <sand4570@stud.kea.dk>
2023-11-20 10:14:23 +0100

    Updated get for task to include query parameters, and a few fixes

--- src/main/kotlin/com/example/todobackend/model/Task.kt
+++ src/main/kotlin/com/example/todobackend/model/Task.kt
@@ -1,22 +1,32 @@
 package com.example.todobackend.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.OneToMany
+import jakarta.persistence.*
 import org.springframework.data.annotation.Id
 import java.sql.Date
 
 @Entity
+@Table(name = "task")
 class Task(
+        @Column(name = "title")
         var title : String,
-        var description : String,
-        var completed : Boolean,
-        var completion : Date,
-        var priority : Priority,
-        @jakarta.persistence.Id @Id
-        @GeneratedValue(strategy = GenerationType.AUTO) var id: Long?,
 
-        @OneToMany(mappedBy = "task", orphanRemoval = true)
+        @Column(name = "description")
+        var description : String,
+
+        @Column(name = "completed")
+        var completed : Boolean,
+
+        @Column(name = "completion")
+        var completion : Date,
+
+        @Column(name = "priority")
+        var priority : Priority,
+
+        @jakarta.persistence.Id @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name = "task_id")
+        var id: Long?,
+
+        @OneToMany(mappedBy = "parent", orphanRemoval = true)
         var subtasks: List<Subtask>
 )
--- src/main/kotlin/com/example/todobackend/repository/TaskRepository.kt
+++ src/main/kotlin/com/example/todobackend/repository/TaskRepository.kt
@@ -1,11 +1,15 @@
 package com.example.todobackend.repository
 
+import com.example.todobackend.model.Priority
 import com.example.todobackend.model.Task
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import java.sql.Date
 
 interface TaskRepository : CrudRepository<Task, Long> {
-
-    fun findAllByCompletion(completion: Date): List<Task>
-
+    @Query("select t from Task t " +
+            "where (t.completion is null or t.completion = #{date}) " +
+            "and (t.priority is null or t.priority = #{priority}) " +
+            "and (t.completed is null or t.completed = #{completed})", nativeQuery = true)
+    fun findAllByParameters(date: Date?, priority: Priority?, completed: Boolean?): List<Task>
 }
