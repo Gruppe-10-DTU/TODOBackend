2945814fb56bbb12166ae2d8fa89542e9e15c9b1
andreasdan <andreas.petersen01@gmail.com>
2024-01-08 14:08:24 +0100

    rebase

---
+++ src/main/kotlin/com/example/todobackend/service/TimeslotService.kt
@@ -0,0 +1,31 @@
+package com.example.todobackend.service
+
+import com.example.todobackend.dto.TimeslotDTO
+import com.example.todobackend.model.Timeslot
+import com.example.todobackend.repository.TimeslotRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TimeslotService(
+    private val timeslotRepository: TimeslotRepository
+) {
+    fun addTimeslot(dto: TimeslotDTO): Timeslot {
+        return timeslotRepository.save(Timeslot(null, dto.name, dto.start, dto.end, emptyList()))
+    }
+
+    fun getAll(): List<Timeslot> {
+        return timeslotRepository.findAll().toList()
+    }
+
+    fun editTimeslot(id: Long, dto: TimeslotDTO): Timeslot {
+        val timeslot = timeslotRepository.findById(id).get()
+        timeslot.name = dto.name
+        timeslot.start = timeslot.start
+        timeslot.end = timeslot.end
+        return timeslotRepository.save(timeslot)
+    }
+
+    fun deleteTimeslot(id : Long) {
+        timeslotRepository.deleteById(id)
+    }
+}
