812626b340c70b8ea6c3d6d462e1b49eb9aea0be
Philip Cramer <s224319@dtu.dk>
2024-01-12 20:05:13 +0100

    changed timeslot to consistently use LocalTime class to represent time

--- src/main/kotlin/com/example/todobackend/dto/TaskDTODeserializer.kt
+++ src/main/kotlin/com/example/todobackend/dto/TaskDTODeserializer.kt
@@ -1,18 +1,14 @@
 package com.example.todobackend.dto
 
 import com.example.todobackend.model.Priority
-import com.example.todobackend.model.Task
 import com.example.todobackend.model.Timeslot
-import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
-import java.sql.Time
 import java.sql.Timestamp
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class TaskDTODeserializer(t: Class<TaskDTO>? = null) : StdDeserializer<TaskDTO>(t) {
@@ -27,8 +23,8 @@ class TaskDTODeserializer(t: Class<TaskDTO>? = null) : StdDeserializer<TaskDTO>(
             val timeslot = jsonNode.get("timeslot")
             val id = timeslot.get("id").asLong()
             val timeslotTitle = timeslot.get("name").asText()
-            val startTime = Time.valueOf(timeslot.get("start").asText())
-            val endTime = Time.valueOf(timeslot.get("end").asText())
+            val startTime = LocalTime.parse(timeslot.get("start").asText())
+            val endTime = LocalTime.parse(timeslot.get("end").asText())
             taskDTO = taskDTO.copy(timeslot = Timeslot(id, timeslotTitle, startTime, endTime, emptyList()))
         }
         return taskDTO
